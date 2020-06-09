edef8da3626e344e03e0381cb53b886a62bedf88
saksham695 <saksham695@gmail.com>
2020-06-09 17:12:18 +0530

    promises

--- app.js
+++ app.js
@@ -1,51 +1,3 @@
-// console.log("Before");
-// getUser("9818773688")
-//   .then((res) => getVideos(res))
-//   .then((res) => getVideoInfo(res))
-//   .then((res) => console.log(res))
-//   .catch((err) => console.log(err));
-
-// function getUser(mobileNo) {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       console.log("Reading from DB :", mobileNo);
-//       resolve({ id: 1, mobileNo: 9818773688, name: "saksham" });
-//     }, 2000);
-//   });
-// }
-// function getVideos(id) {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       console.log("Reading from DB :", id);
-//       resolve(["1", "2", "3"]);
-//       // reject("error here");
-//     }, 2000);
-//   });
-// }
-
-// function getVideoInfo(videoIds) {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       console.log("Reading Video Info DB :", videoIds);
-//       resolve([
-//         {
-//           id: 1,
-//           title: "Good Morning",
-//         },
-//         {
-//           id: 2,
-//           title: "Good AfterNoon",
-//         },
-//         {
-//           id: 3,
-//           title: "Good Night",
-//         },
-//       ]);
-//     }, 2000);
-//   });
-// }
-
-// console.log("After");
 // all Assignment - Suppose you have a user name through that get user_id, through user_id get
 //userRepositories and get commits by repository_id.
 // Solve it using callback and Promise. Pls share both callback and
@@ -53,40 +5,64 @@
 
 const name = "prateek";
 
-getUserID(name, (id) => {
-  getUserRepo(id, (getRepo) => {
-    getRepoCommits(getRepo.repoID, (commits) => {
-      console.log(commits);
-    });
+getUserID(name)
+  .then((res) => {
+    console.log(res);
+    return getUserRepo(res.id);
+  })
+  .then((userRepo) => {
+    console.log(userRepo);
+    return getRepoCommits(userRepo.repoID);
+  })
+  .then((userCommits) => console.log(userCommits))
+  .catch((err) => {
+    console.log(err);
   });
-});
 
-function getUserID(userName, callback) {
-  const user = [
-    { id: 1, name: "saksham" },
-    { id: 2, name: "prateek" },
-    { id: 3, name: "kartik" },
-  ];
-  const getUser = user.find((itr) => itr.name === userName);
-  callback(getUser.id);
+function getUserID(userName) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const user = [
+        { id: 1, name: "saksham" },
+        { id: 2, name: "prateek" },
+        { id: 3, name: "kartik" },
+      ];
+      const getUser = user.find((itr) => itr.name === userName);
+      getUser
+        ? resolve(getUser)
+        : reject(new Error("No user exists with this name"));
+    }, 200);
+  });
 }
 
-function getUserRepo(id, callback) {
-  const user = [
-    { repoID: 1, name: "project1" },
-    { repoID: 2, name: "project2" },
-    { repoID: 3, name: "project3" },
-  ];
-  const getRepo = user.find((itr) => itr.repoID === id);
-  callback(getRepo);
+function getUserRepo(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const user = [
+        { repoID: 1, name: "project1" },
+        { repoID: 2, name: "project2" },
+        { repoID: 3, name: "project3" },
+      ];
+      const getRepo = user.find((itr) => itr.repoID === id);
+      getRepo
+        ? resolve(getRepo)
+        : reject(new Error("Repository not available with this id"));
+    }, 200);
+  });
 }
 
-function getRepoCommits(id, callback) {
-  const user = [
-    { commitID: 1, name: "commit_1" },
-    { commitID: 2, name: "commit_1" },
-    { commitID: 3, name: "commit_1" },
-  ];
-  const getCommit = user.find((itr) => itr.commitID === id);
-  callback(getCommit);
+function getRepoCommits(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const user = [
+        { commitID: 1, name: "commit_1" },
+        { commitID: 4, name: "commit_1" },
+        { commitID: 3, name: "commit_1" },
+      ];
+
+      user
+        ? resolve(user)
+        : reject(new Error("User has no commits with this repoID"));
+    }, 200);
+  });
 }
